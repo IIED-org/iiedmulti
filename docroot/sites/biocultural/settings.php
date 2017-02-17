01e5457b3a47640f38eea84f3d6e43946e0b41d9
Chris Brody <chris@cbrody.com>
2017-02-17 17:28:37 +0000

    DevDesktop multisite export. Cloud db settings block added to settings.php

--- docroot/sites/biocultural/settings.php
+++ docroot/sites/biocultural/settings.php
@@ -636,3 +636,7 @@ if (isset($_SERVER['DEVDESKTOP_DRUPAL_SETTINGS_DIR']) && file_exists($_SERVER['D
   require $_SERVER['DEVDESKTOP_DRUPAL_SETTINGS_DIR'] . '/cld_devcloud_iiedmulti_dev_biocultural.inc';
 }
 // </DDSETTINGS>
+
+if (file_exists('/var/www/site-php')) {
+    require('/var/www/site-php/iiedmulti/biocultural-settings.inc');
+}
