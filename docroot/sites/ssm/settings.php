ac54138e35fc239f997284d65881ecf5f6171acf
Chris Brody <chris@cbrody.com>
2017-02-18 06:01:57 +0000

    DevDesktop multisite export. Cloud db settings block added to settings.php

--- docroot/sites/ssm/settings.php
+++ docroot/sites/ssm/settings.php
@@ -636,3 +636,7 @@ if (isset($_SERVER['DEVDESKTOP_DRUPAL_SETTINGS_DIR']) && file_exists($_SERVER['D
   require $_SERVER['DEVDESKTOP_DRUPAL_SETTINGS_DIR'] . '/cld_devcloud_iiedmulti_dev_ssm.inc';
 }
 // </DDSETTINGS>
+
+if (file_exists('/var/www/site-php')) {
+    require('/var/www/site-php/iiedmulti/ssm-settings.inc');
+}
